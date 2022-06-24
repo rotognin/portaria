001ff30ac9cd906b691a48eb7ef6ac752959ce47
Rodrigo Tognin <rotog@outlook.com>
2022-06-24 18:09:45 -0300

    Verificação de roteamento incorreto

--- index.php
+++ index.php
@@ -10,4 +10,12 @@ $action = (isset($_GET['action'])) ? $_GET['action'] : 'principal';
 $control = (isset($_GET['control'])) ? ucfirst($_GET['control']) : '';
 $funcao = 'Src\\Controller\\' . $control . 'Controller::' . $action;
 
+$controlVerif = 'Src\\Controller\\' . $control . 'Controller';
+
+if (!method_exists($controlVerif, $action)){
+    echo '<h1>Página inexistente...</h1>';
+    echo '<a href=index.php>Clique aqui</a> para retornar ao sistema.';
+    exit;
+}
+
 call_user_func($funcao, $_POST, $_GET);
