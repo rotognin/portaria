664579ffe206ed96b7f5f142a2395f3cb36f5b68
Rodrigo Tognin <rotog@outlook.com>
2024-06-26 10:21:53 -0300

    Ajuste na versão do sistema

--- lib/definicoes.php
+++ lib/definicoes.php
@@ -3,24 +3,24 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('America/Sao_Paulo');
 
-require ('./vendor/autoload.php');
+require('./vendor/autoload.php');
 
 define("DS", DIRECTORY_SEPARATOR);
-define("APP_VERSION", '2022.07.06-001');
+define("APP_VERSION", '2024.06.26-001');
 
 define("NIVEL", array(
     '1' => 'Administrador',
     '2' => 'Usuário Comum'
 ));
 
-define ("STATUS", array(
+define("STATUS", array(
     '0' => 'Ativo',
     '1' => 'Inativo',
     '2' => 'Sob Consulta',
     '3' => 'Bloqueado'
 ));
 
-define ("STATUS_MOVIMENTACAO", array(
+define("STATUS_MOVIMENTACAO", array(
     '0' => 'Em aberto',
     '1' => 'Finalizado',
     '2' => 'Cancelado'
@@ -32,10 +32,10 @@ define("TIPO_PASSAGEM", array(
     '3' => 'Veículos apenas'
 ));
 
-define ("STATUS_ATIVO", 0);
-define ("STATUS_INATIVO", 1);
-define ("STATUS_CONSULTA", 2);
-define ("STATUS_BLOQUEADO", 3);
+define("STATUS_ATIVO", 0);
+define("STATUS_INATIVO", 1);
+define("STATUS_CONSULTA", 2);
+define("STATUS_BLOQUEADO", 3);
 
 define("TIPO_UNIDADE", array(
     '1' => 'Matriz',
